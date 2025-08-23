aa46aadbfe947822c5b022095a242019b1b41237
adtyfathan <adtyfathan04@gmail.com>
2025-08-23 13:11:03 +0700

    first commit

---
+++ app/src/main/java/com/example/todo_app/ui/todo_list/TodoListEvent.kt
@@ -0,0 +1,11 @@
+package com.example.todo_app.ui.todo_list
+
+import com.example.todo_app.data.Todo
+
+sealed class TodoListEvent {
+    data class OnDeleteTodoClick(val todo: Todo): TodoListEvent()
+    data class OnDoneChange(val todo: Todo, val isDone: Boolean): TodoListEvent()
+    object OnUndoDeleteClick: TodoListEvent()
+    data class OnTodoClick(val todo: Todo): TodoListEvent()
+    object OnAddTodoClick: TodoListEvent()
+}
